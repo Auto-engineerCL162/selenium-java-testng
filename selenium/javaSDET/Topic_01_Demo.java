73f528aefc3d795e326b446120baf71cf735e569
Nguyễn Chí Long <nguyenchilong000@gmail.com>
2026-03-09 21:34:21 +0700

    initial First commit

---
+++ selenium/javaSDET/Topic_01_Demo.java
@@ -0,0 +1,11 @@
+package javaSDET;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Topic_01_Demo {
+
+    public static void main(String args) {
+        System.out.println("Hello World");
+    }
+}
